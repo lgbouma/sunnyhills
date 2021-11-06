0218931dfd497bf0f8a38de757ecd0fb618b2f5c
thissop <68932815+thissop@users.noreply.github.com>
2021-11-05 18:52:22 -0700

    nice

---
+++ learning/kepler_experiment/make_new_catalog.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+# choose n random kepler objects that have confirmed planet and were detected with transit method
+# perhaps only choose systems with one planet
+# create a little write-up highlighting results
