35079ba7fc5e7462293952fa6c461df8cbcfbe7d
RyanAxe <97776863+RyanAxe@users.noreply.github.com>
2022-01-17 22:49:01 -0800

    Updated files
    Changed CSV file and deleted python file

--- epochs/ryan/csv+mainscript/IDConverter.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import astrobase
-from astrobase.services.indentifiers import gaiadr2_to_tic
-df=pd.read_csv("Book1.csv")
-IDs = np.array(df["GDR2_ID"])
-tic_id = gaiadr2_to_tic(IDs)
-df["tic_id"] = IDs
