62ae8adbc590768e68b30b55aa678b7b2a929514
thissop <68932815+thissop@users.noreply.github.com>
2022-02-24 10:31:21 -0800

    Update first-check-on-ryan.py

--- personal_epochs/thaddaeus/february_2022/checking-others/first-check-on-ryan.py
+++ personal_epochs/thaddaeus/february_2022/checking-others/first-check-on-ryan.py
@@ -3,20 +3,31 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('./personal_epochs/ryan/Tess-Point/Output.csv')
-out_ids = np.array(df['-1'])[1:]
+df = pd.read_csv('https://raw.githubusercontent.com/HarritonResearchLab/sunnyhills/main/personal_epochs/ryan/Tess-Point/Output.csv')
+
+full_out_ids = np.array(df['-1'])
+
+out_ids = full_out_ids[1:]
+
+all_sectors = np.array(df[' -1'])
 
 set_of_ids = np.array(list(set(out_ids)))
 
 set_of_ids = set_of_ids[np.where(set_of_ids>0)]
-counts = np.array([len(np.where(out_ids==i)[0]) for i in out_ids])
-print(counts)
 
-counts = counts[np.where(counts)]
+counts = np.array([])
 
-counts = counts[np.where(counts<1000)]
+for id in set_of_ids: 
+    df_mask = np.where(full_out_ids==id)
+    count = len(list(set(all_sectors[df_mask])))
+
+    counts = np.append(counts, count)
+    
+    
+print(list(df))
+print(np.mean(counts))
 
 plt.hist(counts)
 plt.xlabel('# sectors')
 plt.ylabel('count')
-plt.savefig('./personal_epochs/thaddaeus/february_2022/checking-others/sectors_hist.png',dpi=150)
+plt.show()
