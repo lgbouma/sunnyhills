ce0e52b7e07ad63a8cdddbbd9daf713a221eeb46
thissop <68932815+thissop@users.noreply.github.com>
2022-03-08 06:21:42 -0800

    :angry:

--- personal_epochs/thaddaeus/march_2022/misc/throwaway.py
+++
@@ -1,4 +0,0 @@
-bap = {'sectors':4}
-bap['sectors']=5
-bap['dad']='jason'
-print(bap)
---
+++ personal_epochs/thaddaeus/march_2022/pipeline_work/sanity_preservation.py
@@ -0,0 +1,5 @@
+import numpy as np
+print(np.__version__)
+import matplotlib.pyplot as plt
+from transitleastsquares import transitleastsquares, cleaned_array
+
