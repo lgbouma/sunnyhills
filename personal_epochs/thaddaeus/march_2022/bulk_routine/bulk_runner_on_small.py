4ed6262a807b216ef83513b50e7b92088459ffea
thissop <68932815+thissop@users.noreply.github.com>
2022-03-15 12:21:48 -0700

    note sure if it's correct

---
+++ personal_epochs/thaddaeus/march_2022/bulk_routine/bulk_runner_on_small.py
@@ -0,0 +1,60 @@
+import os 
+import numpy as np
+import pickle 
+from sunnyhills.pipeline_functions import run_bls
+import matplotlib.pyplot as plt 
+
+data_dir = './data/current/processed/lightcurves'
+
+for file in os.listdir(data_dir):  
+    if file!='.gitkeep': 
+        with open(data_dir+'/'+file,"rb") as f: 
+            content = pickle.load(f)
+
+            stitched_lc = content['stitched_lc'][0]
+            time = stitched_lc['time']
+            flux = stitched_lc['flux']
+
+            results, bls_model, in_transit, stats = run_bls(stitched_lc)
+
+            fig, axs = plt.subplots(1, 2, figsize=(6, 3))
+            fig.tight_layout()
+
+            period = results.period[np.argmax(results.power)]
+
+            ax = axs[0]
+            ax.axvline(period, alpha=0.4, lw=3)
+            for n in range(2, 10):
+                ax.axvline(n*period, alpha=0.4, lw=1, linestyle="dashed")
+                ax.axvline(period / n, alpha=0.4, lw=1, linestyle="dashed")
+            ax.plot(results.period, results.power, color='black', lw=0.5)
+            ax.set(xlim=(0, max(results.period)), ylabel='SDE BLS', xlabel='Period (days)')
+
+            index = np.argmax(results.power)
+            period = results.period[index]
+            t0 = results.transit_time[index]
+            duration = results.duration[index]
+
+            ax = axs[1]
+            x = (time - t0 + 0.5*period) % period - 0.5*period
+            m = np.abs(x) < 0.5
+            ax.scatter(
+                x[m],
+                flux[m],
+                color='blue',
+                s=10,
+                alpha=0.5,
+                zorder=2)
+
+            x = np.linspace(-0.13, 0.13, 1000)
+            f = bls_model.model(x + t0, period, duration, t0)
+            ax.plot(x, f, color='red')
+            #ax.set_xlim(-0.13, 0.13)
+            #plt.ylim(0.9985, 1.00025)
+            ax.set_xlabel("Time from mid-transit (days)")
+            ax.set_ylabel("Flux")
+
+            plt.show()
+
+            plt.clf()
+            plt.close() 
